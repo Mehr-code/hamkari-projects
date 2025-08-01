ce9393fe30cb2935d470cf4fe0e930f3ec6a450a
Mehr-code <papa.molla8@gmail.com>
2025-08-01 16:12:00 +0330

    Create initial Files

---
+++ frontend/Task-Manager/src/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+
+import PrivateRoute from "./routes/PrivateRoute";
+
+import DashBoard from "./pages/Admin/DashBoard";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import CreateTask from "./pages/Admin/CreateTasks";
+import ManageUsers from "./pages/Admin/MangeUsers";
+
+import UserDashBoard from "./pages/User/UserDashBoard";
+import UserTasks from "./pages/User/UserTasks";
+import ViewTaskDetail from "./pages/User/ViewTaskDetail";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<DashBoard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+            <Route path="/admin/mange-users" element={<ManageUsers />} />
+          </Route>
+
+          {/* Users Routes */}
+          <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+            <Route path="/user/dashboard" element={<UserDashBoard />} />
+            <Route path="/user/tasks" element={<UserTasks />} />
+            <Route
+              path="/user/tasks-details/:id"
+              element={<ViewTaskDetail />}
+            ></Route>
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/Task-Manager/src/pages/User/ViewTaskDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewTaskDetail = () => {
+  return <div>ViewTaskDetail</div>;
+};
+
+export default ViewTaskDetail;
