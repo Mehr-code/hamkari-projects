b3cb3a83c73519c088426cd3c5c9ce5b5894205b
Mehr-code <papa.molla8@gmail.com>
2025-08-13 15:51:56 +0330

    "Add API routes:register,login,getInfo,UpdateInfo"

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 const user = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { route } = require("../routes/authRoutes");
 
 // Generate JWT
 const generateToken = (userId) => {
@@ -10,21 +12,137 @@ const generateToken = (userId) => {
 // @desc --> Register A New User
 // @route --> POST /api/auth/register
 // @access --> Public
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl, adminInviteToken } =
+      req.body;
+
+    // Check Is This User Exist
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res
+        .status(400)
+        .json({ message: "کاربری با این ایمیل قبلاً ثبت‌نام کرده است." });
+    }
+
+    // Determine User Role Base Of Token, If True --> Admin , Else --> User
+    let role = "member";
+    if (
+      adminInviteToken &&
+      adminInviteToken == process.env.ADMIN_INVITE_TOKEN
+    ) {
+      role = "admin";
+    }
+
+    // Hashing The Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create A New User
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+      role,
+    });
+
+    // Return User Data By JWT
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "خطای داخلی سرور", error: error.message });
+  }
+};
 
 // @desc --> Login A User
 // @route --> POST /api/auth/login
 // @access --> Public
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "کلمه عبور یا ایمیل وارد شده، نامعتبر است" });
+    }
+
+    // Check Password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ message: "کلمه عبور یا ایمیل وارد شده، نامعتبر است" });
+    }
+
+    // Return User Data By JWT
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "خطای داخلی سرور", error: error.message });
+  }
+};
 
 // @desc --> Get A Profile User
 // @route --> POST /api/auth/profile
 // @access --> Private (Requires JWT)
-const getUserProfile = async (req, res) => {};
+const getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "کاربری با این مشخصات یافت نشد" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "خطای داخلی سرور", error: error.message });
+  }
+};
 
 // @desc --> Update A Profile User
 // @route --> POST /api/auth/profile
 // @access --> Private (Requires JWT)
-const updateUserProfile = async (req, res) => {};
+const updateUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
 
-module.exports = { registerUser, loginUser, getUserProfile, updateUserProfile };
+    if (!user) {
+      return res.status(404).json({ message: "کاربری مورد نظر یافت نشد" });
+    }
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      role: updatedUser.role,
+      token: generateToken(updatedUser._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "خطای داخلی سرور", error: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser, getUserInfo, updateUserInfo };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const {
   registerUser,
   loginUser,
-  getUserProfile,
-  updateUserProfile,
+  getUserInfo,
+  updateUserInfo,
 } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 
@@ -12,7 +12,17 @@ const router = express.Router();
 // Auth Routes
 router.post("/register", registerUser); // Registering A User
 router.post("/login", loginUser); // Login User
-router.get("/profile", protect, updateUserProfile); // Update A Profile User
-router.get("/profile", protect, getUserProfile); // Get A profile User
+router.get("/info", protect, getUserInfo); // Get A User Info
+router.put("/info", protect, updateUserInfo); // Update A User Info
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "هیچ عکسی بارگذاری نشده" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
 
 module.exports = router;
